34d74c180a020c8074c9d519b57446de67b4738f
Umar Muzammil <umarmuzammil@hotmail.com>
2017-11-28 00:41:42 +0500

    LocalUpload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon;
+
+namespace com.Basket.Graystork {
+    public class GameManager : PunBehaviour {
+
+        //CallBacks
+        public void LeftRoom() {
+            SceneManager.LoadScene(0);
+        }
+
+        public override void OnPhotonPlayerConnected(PhotonPlayer other) {
+            Debug.Log(other.NickName);
+            if (PhotonNetwork.isMasterClient) {
+                LoadMultiplayerScene();
+            }
+        }
+
+        public override void OnPhotonPlayerDisconnected(PhotonPlayer other) {
+            if (PhotonNetwork.isMasterClient) {
+                LoadMultiplayerScene();
+            }
+        }
+        void LoadMultiplayerScene() {
+            if (!PhotonNetwork.isMasterClient) {
+                Debug.Log("You are not Master Client");
+            }
+
+            PhotonNetwork.LoadLevel("MultiplayerGameScene");
+        }
+        //CallBacks End
+
+        public void LeaveRoom() {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        
+      
+    }
+}
---
+++ Assets/Scripts/PlayerInputField.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(InputField))]
+public class PlayerInputField : MonoBehaviour {
+
+    static string playerNamePrefKey = "Player";
+    void Start () {
+
+        string defaultName = "";
+        InputField inputField = GetComponent<InputField>();
+
+        if(inputField.text != null) {
+            if(PlayerPrefs.HasKey("playerNamePrefKey")) {
+                defaultName = PlayerPrefs.GetString("playerNamePrefKey");
+                inputField.text = defaultName;
+            }
+        }
+
+        PhotonNetwork.playerName = defaultName;
+	}
+
+    public void SetPlayerName(string value) {
+       
+        PhotonNetwork.playerName = value + " "; 
+        PlayerPrefs.SetString(playerNamePrefKey, value);
+    }
+}
